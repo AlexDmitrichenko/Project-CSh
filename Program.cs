7670ce9e8e389be17db66d22b8a6f79b59c034b7
Alex <best1972@list.ru>
2023-02-14 16:36:29 +0300

    delete useless

--- Program.cs
+++ Program.cs
@@ -1,18 +1,4 @@
 ﻿string[] signs = {"hello", "2", "world", ":-)"};
-// void NewArray(string[] signs)
-// {
-// for (int i = 0; i < signs.Length; i++)
-// {
-//     if (signs[i].Length <= 3) signs[i] = signs[i];
-// }
-// }
-// // int size = 0;
-// // while (size <= signs.Length) 
-// // {
-// //     if (signs[size].Length < 3);
-// // string[size] newsings = signs[size]
-// // }
-// NewArray (signs);
 PrintArray(signs);
 void PrintArray(string[] signs)
 {
