b9cf6046a7d8feab969e327d8c449cc209bc6f18
Alex <best1972@list.ru>
2023-02-14 16:04:27 +0300

    step 1 Count

--- Program.cs
+++ Program.cs
@@ -1,11 +1,19 @@
 ﻿string[] signs = {"hello", "2", "world", ":-)"};
+
+// int size = 0;
+// while (size <= signs.Length) 
+// {
+//     if (signs[size].Length < 3);
+// string[size] newsings = signs[size]
+// }
 PrintArray(signs);
 void PrintArray(string[] signs)
 {
     int count = signs.Length;
-    for (int i = 0; i < count; i++)
+    for (int x = 0; x < count; x++)
     {
-        System.Console.Write($"{signs[i]} ");
+        System.Console.Write($"{signs[x]} ");
     }
     System.Console.WriteLine();
 }
+
