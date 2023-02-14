934aaca8cb12ac81167313306ebbfa80c1429f04
Alex <best1972@list.ru>
2023-02-14 20:13:07 +0300

    made variant 2 wrong

--- Program.cs
+++ Program.cs
@@ -1,13 +1,24 @@
-﻿string[] signs = {"hello", "2", "world", ":-)"};
-Array(signs);
-//Console.WriteLine($"[{String.Join(", ", signs)}]");
-void Array(string[] signs)
+﻿string[] signs = { "hello", "2", "world", ":-)" };
+string [] newsigns = array(signs);
+Console.WriteLine($"[{String.Join(", ", newsigns)}]");
+// Array(signs);
+// //Console.WriteLine($"[{String.Join(", ", signs)}]");
+// void Array(string[] signs)
+// {
+//     int count = signs.Length;
+//     for (int i = 0; i < count; i++)
+//     {
+//         if (signs[i].Length <= 3) System.Console.Write($"{signs[i]} ");//String.Join("; ",) 
+//     }
+//     System.Console.WriteLine();
+// }
+string[] array (string[] signs)
 {
-    int count = signs.Length;
-    for (int i = 0; i < count; i++)
-    {
-        if (signs[i].Length <= 3) System.Console.Write($"{signs[i]} ");//String.Join("; ",) 
-    }
-    System.Console.WriteLine();
+    string[] array = new string[signs.Length];
+  for (int i = 0; i < signs.Length; i++)
+  {
+    if (signs[i].Length <= 3) array[i] = signs[i];
+  }
+ return array;
 }
 
