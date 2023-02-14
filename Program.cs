84bf6364bcd5ea543f0bbbafb7e3918aa0688e3d
Alex <best1972@list.ru>
2023-02-14 16:28:20 +0300

    step 2 method

--- Program.cs
+++ Program.cs
@@ -1,18 +1,25 @@
 ﻿string[] signs = {"hello", "2", "world", ":-)"};
-
-// int size = 0;
-// while (size <= signs.Length) 
+// void NewArray(string[] signs)
 // {
-//     if (signs[size].Length < 3);
-// string[size] newsings = signs[size]
+// for (int i = 0; i < signs.Length; i++)
+// {
+//     if (signs[i].Length <= 3) signs[i] = signs[i];
 // }
+// }
+// // int size = 0;
+// // while (size <= signs.Length) 
+// // {
+// //     if (signs[size].Length < 3);
+// // string[size] newsings = signs[size]
+// // }
+// NewArray (signs);
 PrintArray(signs);
 void PrintArray(string[] signs)
 {
     int count = signs.Length;
-    for (int x = 0; x < count; x++)
+    for (int i = 0; i < count; i++)
     {
-        System.Console.Write($"{signs[x]} ");
+        if (signs[i].Length <= 3) System.Console.Write($"{signs[i]} ");
     }
     System.Console.WriteLine();
 }
