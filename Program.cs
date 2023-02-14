0046aa31b448d2187ac79075bd1840be04485a3b
Alex <best1972@list.ru>
2023-02-14 15:07:22 +0300

    step 1 Print

--- Program.cs
+++ Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿string[] signs = {"hello", "2", "world", ":-)"};
+PrintArray(signs);
+void PrintArray(string[] signs)
+{
+    int count = signs.Length;
+    for (int i = 0; i < count; i++)
+    {
+        System.Console.Write($"{signs[i]} ");
+    }
+    System.Console.WriteLine();
+}
