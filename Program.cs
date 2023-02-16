91ef3f73bc3589ec10bbf1d91578b1af99fd05fd
Alex <best1972@list.ru>
2023-02-16 15:25:59 +0300

    end

--- Program.cs
+++ Program.cs
@@ -1,23 +1,23 @@
 ﻿// string[] signs = {"hello", "2", "world", ":-)"};
 // string[] signs =  {"1234", "1567", "-2", "computer science"};
-string[] signs =  {"Russia", "Russia", "Kazan"};
-int size = 0;
-for (int x = 0; x < signs.Length; x++)
-{
-    if (signs[x].Length <= 3) size++;
-}
-Console.WriteLine($"[{String.Join(", ", Arraysigns(size))}]");
-string[] Arraysigns(int size)
-{
-    string[] array = new string[size];
-    int y = 0;
-    for (int i = 0; i < signs.Length; i++)
-    {
-        if (signs[i].Length <= 3)
-        {
-            array[y] = signs[i]; y++;
-        }
-    }
-    return array;
-}
+// string[] signs =  {"Russia", "Russia", "Kazan"};
+// int size = 0;
+// for (int x = 0; x < signs.Length; x++)
+// {
+//     if (signs[x].Length <= 3) size++;
+// }
+// Console.WriteLine($"[{String.Join(", ", Arraysigns(size))}]");
+// string[] Arraysigns(int size)
+// {
+//     string[] array = new string[size];
+//     int y = 0;
+//     for (int i = 0; i < signs.Length; i++)
+//     {
+//         if (signs[i].Length <= 3)
+//         {
+//             array[y] = signs[i]; y++;
+//         }
+//     }
+//     return array;
+// }
 
