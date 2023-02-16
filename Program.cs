54e7a3cb359ec6cf44fc7ac60547053483fb9bd4
Alex <best1972@list.ru>
2023-02-16 15:13:39 +0300

    made task

--- Program.cs
+++ Program.cs
@@ -1,31 +1,21 @@
-﻿string[] signs = {"hello", "2", "world", ":-)"};
-// int size = 0;
-// for (int x = 0; x < signs.Length; x++)
-// {
-//   if (signs[x].Length <= 3) size = size + 1;
-//   else size = size * 1;
-// }
-// Console.Write($"{size} ");
-// string [] newsigns = array(signs);
-// Console.WriteLine($"[{String.Join(", ", newsigns)}]");
-// Array(signs);
-// //Console.WriteLine($"[{String.Join(", ", signs)}]");
-// void Array(string[] signs)
-// {
-//     int count = signs.Length;
-//     for (int i = 0; i < count; i++)
-//     {
-//         if (signs[i].Length <= 3) System.Console.Write($"{signs[i]} ");
-//     }
-//     System.Console.WriteLine();
-// }
-// string[] array (string[] signs)
-// {
-//   string[] array = new string[size];
-//   for (int i = 0; i < signs.Length; i++)
-//   {
-//     if (signs[i].Length <= 3) array[i] = signs[i];
-//   }
-//  return array;
-// }
+﻿string[] signs = { "hello", "2", "world", ":-)" };
+int size = 0;
+for (int x = 0; x < signs.Length; x++)
+{
+    if (signs[x].Length <= 3) size++;
+}
+Console.WriteLine($"[{String.Join(", ", Arraysigns(size))}]");
+string[] Arraysigns(int size)
+{
+    string[] array = new string[size];
+    int y = 0;
+    for (int i = 0; i < signs.Length; i++)
+    {
+        if (signs[i].Length <= 3)
+        {
+            array[y] = signs[i]; y++;
+        }
+    }
+    return array;
+}
 
